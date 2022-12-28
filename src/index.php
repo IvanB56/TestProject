7013e5b02f3b1cdc15c205ba06c00866ac9464dc
Иван <burack.iw@yandex.ru>
2022-12-28 21:56:00 +0300

    add html and styles

---
+++ src/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+<header>
+    <div class="img">
+        <a href="https://lavka-monet.ru/" title="Монетная лавка">
+            <img src="static/logo.png" alt="logo">
+        </a>
+    </div>
+    <div class="info">
+        <div class="phone">8 800 777 40 39</div>
+        <div>
+            <p>*звонок на территории РФ бесплатный</p>
+            <p>РАБОТАЕМ С 9:00 ДО 21:00 ПО МСК</p>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="block-1">
+        <div class="bg-image"></div>
+        <div class="form">
+            <p>Закажите уникальную коллекцию монет</p>
+            <h2>коллекция монет “Великие женщины”</h2>
+            <p>Роскошное пополнение личной коллекции или интересный тематический подарок для друзей и близких</p>
+            <p>Оставь заявку и получи альбом в подарок!</p>
+            <form>
+                <div class="input-group">
+                    <label>
+                        <input type="text" maxlength="12" placeholder="+7 (999) 999-99-99">
+                    </label>
+                </div>
+                <div class="submit">
+                    <button type="button">Заказать</button>
+                </div>
+            </form>
+            <p>Нажимая на кнопку, вы даёте согласие на обработку персональных данных и соглашаетесь с политикой конфеденциальности</p>
+        </div>
+    </div>
+</main>
+</body>
+</html>
