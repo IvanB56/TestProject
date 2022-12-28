8f6ce7b01027230b79b247eef2947fcb9264bd48
Иван <burack.iw@yandex.ru>
2022-12-28 20:23:23 +0300

    add html and styles

--- index.php
+++ index.php
@@ -6,8 +6,26 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Test</title>
+    <link rel="stylesheet" href="./src/styles/styles.css">
 </head>
 <body>
-    <h1>Test Project</h1>
+<header>
+    <div class="img">
+        <img src="./src/static/logo.png" alt="logo">
+    </div>
+    <div class="info">
+        <div class="phone">8 800 777 40 39</div>
+        <div>
+            <span>*звонок на территории РФ бесплатный</span>
+            <p>РАБОТАЕМ С 9:00 ДО 21:00 ПО МСК</p>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="block">
+        <div class="bg-image"></div>
+
+    </div>
+</main>
 </body>
 </html>
